7d4ef0d4c15c72a1b5317e07de540d3610e8b4a3
Vishal sahu <vishalsahu1230@gmail.com>
2025-07-31 05:29:02 +0530

    Initial commit

--- index.js
+++ index.js
@@ -25,11 +25,7 @@ app.use('/api/search', searchRoutes);
 // app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.error(err));
